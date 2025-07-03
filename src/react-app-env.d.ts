8a043b2311c876ae8bb75c2a322895d086930346
Clever Dzifa Derenbach <clevertedeku@gmail.com>
2025-07-03 12:37:55 +0200

    implement stripe payment integration for MVP workflow

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+interface ImportMetaEnv {
+	readonly REACT_APP_STRIPE_PUBLISHABLE_KEY: string;
+	// add other env variables here as needed
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
